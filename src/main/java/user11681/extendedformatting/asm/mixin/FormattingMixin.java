c70aac32c02cac20f8668a4175284fe0dc851b23
user11681 <username11681@gmail.com>
2020-08-23 19:58:47 +0100

    fix duplicate map keys

--- src/main/java/user11681/extendedformatting/asm/mixin/FormattingMixin.java
+++ src/main/java/user11681/extendedformatting/asm/mixin/FormattingMixin.java
@@ -1,8 +1,14 @@
 package user11681.extendedformatting.asm.mixin;
 
+import it.unimi.dsi.fastutil.objects.Object2ReferenceArrayMap;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import user11681.extendedformatting.Format;
 import user11681.extendedformatting.asm.access.CustomFormattingAccess;
 
@@ -16,6 +22,21 @@ public abstract class FormattingMixin implements CustomFormattingAccess {
 
     @Unique
     public boolean custom;
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Redirect(method = "<clinit>",
+              at = @At(value = "INVOKE",
+                       target = "Ljava/util/stream/Stream;collect(Ljava/util/stream/Collector;)Ljava/lang/Object;"))
+    private static Object fixDuplicates(final Stream<Formatting> stream, Collector<Formatting, Object, Map<String, Formatting>> collector) {
+        final Formatting[] formattings = stream.toArray(Formatting[]::new);
+        final Map<String, Formatting> byName = new Object2ReferenceArrayMap<>(formattings.length);
+
+        for (final Formatting formatting : formattings) {
+            byName.put(formatting.getName(), formatting);
+        }
+
+        return byName;
+    }
 
     @Override
     @Unique
